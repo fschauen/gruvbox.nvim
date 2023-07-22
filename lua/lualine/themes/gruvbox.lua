dba1edef828170304b3dca2ef849db6fcc7563e4
Fernando Schauenburg <dev@schauenburg.me>
2023-07-23 01:10:05 +0200

    Make the status line less dark

--- lua/lualine/themes/gruvbox.lua
+++ lua/lualine/themes/gruvbox.lua
@@ -2,21 +2,21 @@ local C = require'gruvbox'.colors()
 
 return {
   normal = {
-    a = { fg = C.bg0,  bg = C.blue_faded, bold = true },
+    a = { fg = C.bg0,  bg = C.blue, bold = true },
     b = { fg = C.fg2,  bg = C.bg3 },
     c = { fg = C.fg3,  bg = C.bg2 },
   },
   insert = {
-    a = { fg = C.bg0,  bg = C.green_faded, bold = true },
+    a = { fg = C.bg0,  bg = C.green, bold = true },
   },
   visual = {
-    a = { fg = C.bg0,  bg = C.purple_faded, bold = true },
+    a = { fg = C.bg0,  bg = C.purple, bold = true },
   },
   replace = {
-    a = { fg = C.bg0,  bg = C.red_faded, bold = true },
+    a = { fg = C.bg0,  bg = C.red, bold = true },
   },
   command = {
-    a = { fg = C.bg0,  bg = C.yellow_faded, bold = true },
+    a = { fg = C.bg0,  bg = C.yellow, bold = true },
   },
   inactive = {
     a = { fg = C.fg3,  bg = C.bg3 },

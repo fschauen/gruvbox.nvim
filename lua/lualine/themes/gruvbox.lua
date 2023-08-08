53609156c98361fa8fb84078255eb2bffe817201
Fernando Schauenburg <dev@schauenburg.me>
2023-08-08 05:22:29 +0200

    Tweak lualine colors

--- lua/lualine/themes/gruvbox.lua
+++ lua/lualine/themes/gruvbox.lua
@@ -1,27 +1,26 @@
-local C = require'gruvbox'.colors()
+local C = require 'gruvbox'.colors()
 
 return {
   normal = {
-    a = { fg = C.bg0,  bg = C.blue, bold = true },
-    b = { fg = C.fg2,  bg = C.bg3 },
-    c = { fg = C.fg3,  bg = C.bg2 },
+    a = { fg = C.bg0, bg = C.blue, bold = true },
+    b = { fg = C.fg2, bg = C.bg2 },
+    c = { fg = C.fg2, bg = C.bg1 },
   },
   insert = {
-    a = { fg = C.bg0,  bg = C.green, bold = true },
+    a = { fg = C.bg0, bg = C.green, bold = true },
   },
   visual = {
-    a = { fg = C.bg0,  bg = C.purple, bold = true },
+    a = { fg = C.bg0, bg = C.purple, bold = true },
   },
   replace = {
-    a = { fg = C.bg0,  bg = C.red, bold = true },
+    a = { fg = C.bg0, bg = C.red, bold = true },
   },
   command = {
-    a = { fg = C.bg0,  bg = C.yellow, bold = true },
+    a = { fg = C.bg0, bg = C.yellow, bold = true },
   },
   inactive = {
-    a = { fg = C.fg3,  bg = C.bg3 },
-    b = { fg = C.fg4,  bg = C.bg2 },
-    c = { fg = C.gray, bg = C.bg1 },
+    a = { fg = C.fg4, bg = C.bg2 },
+    b = { fg = C.fg4, bg = C.bg2 },
+    c = { fg = C.fg4, bg = C.bg1 },
   },
 }
-
